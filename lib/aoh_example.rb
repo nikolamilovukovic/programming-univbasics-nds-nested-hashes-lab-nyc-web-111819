ef2cc7ae0dca52bd021bf171bd72314b9703951d
Nikola Milovukovic <nikolamilovukovic@gmail.com>
2019-11-09 16:53:47 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -15,13 +15,17 @@ def assembled_aoh
     BRUCE_G
     ]
   
-  
-  
   # Build an array that contains (or, "nests") the constants into a single
   # Array. Ruby constants are denoted by ALL_CAPS
 end
 
 def literal_aoh
+  
+  
+  
+  
+  
+  
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
 end
